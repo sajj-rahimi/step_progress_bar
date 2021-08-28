642e52e7e088a7a578ea50a7b3756b9a70919528
sajjad rahimi <sajjadrahimi4@gmail.com>
2021-08-28 23:51:21 +0430

    first components setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,117 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main-container">
+    <div class="progress-container">
+      <div class="progress" v-for="(node, index) in nodes" :id="index" :key="index">
+        <div  class="node" :class="[`node-${index}`,{'start-node active': index===0} , {'last-node': index+1===nodes.length}]" :id="index">
+          <div class="node-circle">{{index}}</div>
+          <div class="node-content">{{node.content}}</div>
+          <div class="node-tooltip" 
+            :style="[{top: index%2===0 ? '-150%' : '200%'},
+                    {transform:index%2===0 ? 'translateY(30px) translateX(-50%)' : 'translateY(-30px) translateX(-50%)'}
+                    ]">
+              {{node.tooltip}}
+          </div>
+        </div>
+        <div class="progress-bar" :class="[`bar-${index}`]" v-if="(index + 1)!==(nodes.length)" :id="index"></div>
+      </div>
+    </div>
+      <div class="operations">
+        <button class="next" @click="next()">next</button>
+        <button class="prev" @click="prev()">prev</button>
+        <button class="restart" @click="restart()">restart</button>
+      </div>
   </div>
 </template>
 
 <script>
+import '@/assets/index.scss'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data(){
+    return{
+       nodes:[
+          {
+            content:'salam',
+            tooltip:'chetori?'
+          },
+          {
+            content:'salam',
+            tooltip:'chetori?'
+          },
+          {
+            content:'salam',
+            tooltip:'chetori?'
+          },
+          {
+            content:'salam',
+            tooltip:'chetori?'
+          },
+          {
+            content:'salam',
+            tooltip:'chetori?'
+          },
+          {
+            content:'salam',
+            tooltip:'chetori?'
+          }
+        ]
+    }
+  },
+  methods:{
+   next(){
+      let currNode = document.querySelector('.node.active')
+      if(currNode){
+        let currNodeIndex = Number(currNode.getAttribute('id'))
+
+        currNode.classList.remove('active')
+        currNode.classList.add('checked')
+        currNode.firstElementChild.innerHTML=''
+
+        if(currNodeIndex!==this.nodes.length-1){
+          let nextNode = document.querySelector(`.node.node-${currNodeIndex+1}`)
+          nextNode.classList.add('active')
+
+          let currBar = document.querySelector(`.bar-${currNodeIndex}`)
+          currBar.classList.add('checked')
+        }
+      } 
+   },
+   prev(){
+      let currNode = document.querySelector('.node.active') ? document.querySelector('.node.active') : document.querySelector('.last-node')
+      if(currNode){
+        let currNodeIndex = Number(currNode.getAttribute('id'))
+
+        if(currNodeIndex!==0){
+          currNode.classList.remove('active')
+          currNode.classList.remove('checked')
+          currNode.firstElementChild.innerHTML=''
+          currNode.firstElementChild.innerHTML=currNodeIndex
+
+          let prevNode = document.querySelector(`.node.node-${currNodeIndex-1}`)
+          prevNode.classList.remove('checked')
+          prevNode.firstElementChild.innerHTML=''
+          prevNode.firstElementChild.innerHTML=currNodeIndex-1
+          prevNode.classList.add('active')
+
+          let prevBar = document.querySelector(`.bar-${currNodeIndex-1}`)
+          prevBar.classList.remove('checked')
+        }
+      } 
+   },
+   restart(){
+      document.querySelectorAll('.node').forEach((node , index)  => {
+          node.classList.remove('checked')
+          node.classList.remove('active')
+          node.firstElementChild.innerHTML = index
+          if(node.classList.contains('start-node')){
+            node.classList.add('active')
+          }
+      })
+      document.querySelectorAll('.progress-bar').forEach(bar=>{
+        bar.classList.remove('checked')
+      })
+   }
+ }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
