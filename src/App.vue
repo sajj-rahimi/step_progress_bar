c926b0632a6634d05a25f7fd4023ff962609f310
sajjad rahimi <sajjadrahimi4@gmail.com>
2021-09-10 17:49:08 +0430

    feat: props and function

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,102 @@
 <template>
-  <HelloWorld ref="progress" />
+<div class="main-container">
+  <step-progress-bar :options="options" ref="progress" />
+  <div class="operations">
+        <div class="button-container">
+          <button class="next" @click="next()">next</button>
+          <button class="prev" @click="prev()">prev</button>
+          <button class="restart" @click="restart()">restart</button>
+        </div>
+        <div class="input-container">
+          <div class="input-group">
+            <label for="barHeight-input">bar height :</label>
+            <input type="number" v-model="options.barHeight" name="" id="barHeight-input">
+          </div>
+          <div class="input-group">
+            <label for="node-width-input">node width :</label>
+            <input type="number" v-model="options.nodeWidth" name="" id="node-width-input">
+          </div>
+          <div class="input-group">
+            <label for="nodeHeight-input">node height :</label>
+            <input type="number" v-model="options.nodeHeight" name="" id="nodeHeight-input">
+          </div>
+        </div>
+  </div>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StepProgressBar from './components/StepProgressBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StepProgressBar
+  },
+  data(){
+    return{
+      options:{
+          maxWidth:'100%',
+          nodeWidth:40,
+          nodeHeight:40,
+          barHeight:3,
+          showTooltip:true,
+          showContent:true,
+          barType:'dashed',
+          onNext:this.onNext,
+          onFinish:this.onFinish,
+          nodes:[
+              {
+                content:'salam salam salamsalam salam salam ',
+                tooltip:'salam slam salam salam salam salam salam salam'
+              },
+              {
+                content:'salam',
+                tooltip:'chetori?'
+              },
+              {
+                content:'salam',
+                tooltip:'chetori?'
+              },
+              {
+                content:'salam',
+                tooltip:'chetori?'
+              },
+              {
+                content:'salam',
+                tooltip:'chetori?'
+              },
+              {
+                content:'salam',
+                tooltip:'chetori?'
+              }
+            ]
+      }
+    }
   },
   created(){
-    setTimeout(() => {
-       this.$refs.progress.next()
-    }, 20);
+    // setTimeout(() => {
+    //    this.$refs.progress.next()
+    // }, 20);
    
+  },
+  methods:{
+    onNext(){
+      console.log('onNext')
+    },
+    onFinish(){
+      console.log('finshed')
+    },
+    next(){
+      this.$refs.progress.next()
+    },
+    prev(){
+      this.$refs.progress.prev()
+    },
+    restart(){
+      this.$refs.progress.restart()
+    }
   }
 }
 </script>
@@ -22,10 +104,5 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0;
 }
 </style>
---
+++ src/components/StepProgressBar.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="progress-container" :style="{maxWidth:options.maxWidth}">
+      <div class="progress" v-for="(node, index) in options.nodes" :id="index" :key="index">
+        <div  class="node" 
+        :style="{width:options.nodeWidth+'px', height:options.nodeHeight + 'px'}"
+        :class="[`node-${index}`,{'start-node active': index===0} , {'last-node': index+1===options.nodes.length}]" :id="index">
+          <div class="node-circle" :id="index">{{index+1}}</div>
+          <div v-if="options.showContent" class="node-content"><p>{{node.content}}</p></div>
+          <div v-if="options.showTooltip" class="tooltip-container">
+            <div class="node-tooltip" 
+              :class="[`node-tooltip-${index}`, index%2===0 ? 'node-tooltip-top' : 'node-tooltip-down']">
+                <p class="tooltip-text" style="position:relative"> {{node.tooltip}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="progress-bar"  :class="[`bar-${index}`]" v-if="(index + 1)!==(options.nodes.length)" :id="index">
+          <div class="progress-bar-active" :style="{borderTop:options.barHeight + `px ${options.barType} #bbbbbb`}">
+          </div>
+          <div class="progress-bar-checked" :style="{borderTop:options.barHeight + 'px solid #61B15A'}"></div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import '@/assets/index.scss'
+export default {
+  name: 'StepProgressBar',
+  props:{
+    options:{type:Object, default:function(){
+          return {
+            maxWidth:'100%',
+            nodeWidth:40,
+            nodeHeight:40,
+            barHeight:3,
+            showTooltip:true,
+            showContent:true,
+            barType:'dashed',
+            nodes:[
+                {
+                  content:'salam salam salamsalam salam salam ',
+                  tooltip:'salam slam salam salam salam salam salam salam'
+                },
+                {
+                  content:'salam',
+                  tooltip:'chetori?'
+                },
+                {
+                  content:'salam',
+                  tooltip:'chetori?'
+                },
+                {
+                  content:'salam',
+                  tooltip:'chetori?'
+                },
+                {
+                  content:'salam',
+                  tooltip:'chetori?'
+                },
+                {
+                  content:'salam',
+                  tooltip:'chetori?'
+                }
+              ]
+          }
+        }
+     }
+  },
+  data(){
+    return{
+    
+    }
+  },
+  created(){
+    setTimeout(this.handleHover , 20)
+  },
+  methods:{
+   next(){
+      if(this.options?.beforeNext){
+        this.options.onNext()
+      }
+      let currNode = document.querySelector('.node.active')
+      if(currNode){
+        let currNodeIndex = Number(currNode.getAttribute('id'))
+
+        currNode.classList.remove('active')
+        currNode.classList.add('checked')
+        currNode.firstElementChild.innerHTML=''
+
+        if(currNodeIndex!==this.options.nodes.length-1){
+          let nextNode = document.querySelector(`.node.node-${currNodeIndex+1}`)
+          nextNode.classList.add('active')
+
+          let currBar = document.querySelector(`.bar-${currNodeIndex}`)
+          currBar.classList.add('checked')
+        } else if (this.options?.onFinish) {
+          this.options.onFinish()
+        }
+      } 
+   },
+   prev(){
+      let currNode = document.querySelector('.node.active') ? document.querySelector('.node.active') : document.querySelector('.last-node')
+      if(currNode){
+        let currNodeIndex = Number(currNode.getAttribute('id'))
+
+        if(currNodeIndex!==0){
+          currNode.classList.remove('active')
+          currNode.classList.remove('checked')
+          currNode.firstElementChild.innerHTML=''
+          currNode.firstElementChild.innerHTML=currNodeIndex+1
+
+          let prevNode = document.querySelector(`.node.node-${currNodeIndex-1}`)
+          prevNode.classList.remove('checked')
+          prevNode.firstElementChild.innerHTML=''
+          prevNode.firstElementChild.innerHTML=currNodeIndex
+          prevNode.classList.add('active')
+
+          let prevBar = document.querySelector(`.bar-${currNodeIndex-1}`)
+          prevBar.classList.remove('checked')
+        }
+      } 
+   },
+   restart(){
+      document.querySelectorAll('.node').forEach((node , index)  => {
+          node.classList.remove('checked')
+          node.classList.remove('active')
+          node.firstElementChild.innerHTML = index
+          if(node.classList.contains('start-node')){
+            node.classList.add('active')
+          }
+      })
+      document.querySelectorAll('.progress-bar').forEach(bar=>{
+        bar.classList.remove('checked')
+      })
+   },
+   handleHover(){
+     if(this.options.showTooltip){
+       document.querySelectorAll('.node-circle').forEach(node=>{
+          const index = node.getAttribute('id')
+          node.addEventListener('mouseover', function(){
+            document.querySelectorAll('.node-tooltip').forEach(item=>{item.classList.remove('remove-tooltip'),item.classList.remove('show-tooltip')})
+              document.querySelector(`.node-tooltip.node-tooltip-${index}`).classList.add('show-tooltip')
+          })
+          node.addEventListener('mouseleave', function(){
+              document.querySelectorAll('.node-tooltip').forEach(item=>{item.classList.remove('remove-tooltip'),item.classList.remove('show-tooltip')})
+              document.querySelector(`.node-tooltip.node-tooltip-${index}`).classList.add('remove-tooltip')
+          })
+      })
+     }
+   }
+ }
+}
+</script>
+
